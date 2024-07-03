23ffc76bb5e509687ea81787a3e818e0d7c186c3
anna-marshalova <missmarshal22@gmail.com>
2024-07-03 17:05:25 +0700

    edit main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.assistant.assistant import Assistant
+from src.model.pipeline import TextGenerationPipeline
 from src.paths import HOTEL_DATA_PATH
 
 if __name__ == "__main__":
-    hotel_assistant = Assistant(HOTEL_DATA_PATH / "test_hotels.json")
+    pipe = TextGenerationPipeline()
+    hotel_assistant = Assistant(pipe, HOTEL_DATA_PATH / "test_hotels.json")
     hotel_assistant.run()
